bdd3c0b6f0b3b20f123ce6c4f95ee50fb18a19fe
Artem Penteskul <artemiy.penteskul@gmail.com>
2024-01-21 17:46:50 +0200

    styled delete button in todoitem

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -5,6 +5,7 @@ export default function TodoItem({ item }) {
     return (
         <div className={styles.itemContainer}>
             <div className={styles.item} key={item}>{item}</div>
+            <button className={styles.deleteBtn}>X</button>
         </div>
     );
 }

2af3ca23817d3d2e4be268eb3e81a43b6d015e47
Artem Penteskul <artemiy.penteskul@gmail.com>
2024-01-21 16:06:06 +0200

    refactoring

---
+++ src/components/Form.jsx
@@ -0,0 +1,18 @@
+import {useState} from 'react';
+
+export default function Form({ todos, setTodos }) {
+    const [todo, setTodo] = useState('');
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        setTodos([...todos, todo]); // spread operator ... copies all previous items
+        setTodo('');
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input onChange={e => setTodo(e.target.value)} value={todo} type="text"/>
+            <button type="submit">Add</button>
+        </form>
+    );
+}
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,28 +1,16 @@
-import { useState } from 'react';
+import {useState} from 'react';
 
-import TodoItem from './TodoItem.jsx';
+import Form from './Form.jsx';
+import TodoList from './TodoList.jsx';
 
 
 export default function Todo() {
-    const [todo, setTodo] = useState('');
     const [todos, setTodos] = useState([]);
-
-    function handleSubmit(e) {
-        e.preventDefault();
-        setTodos([...todos, todo]); // spread operator ... copies all previous items
-        setTodo('');
-    }
 
     return (
         <div>
-
-            <form onSubmit={handleSubmit}>
-                <input onChange={e => setTodo(e.target.value)} value={todo} type="text"/>
-                <button type="submit">Add</button>
-            </form>
-
-            {todos.map(item => <TodoItem key={item} item={item} />)}
-
+            <Form todos={todos} setTodos={setTodos} />
+            <TodoList todos={todos} />
         </div>
     );
 }

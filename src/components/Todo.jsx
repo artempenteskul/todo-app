2ba2cd20f763e5b83d428127ec3b5f04682a12cb
Artem Penteskul <artemiy.penteskul@gmail.com>
2024-01-21 14:38:56 +0200

    added submit btn handling

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,10 +2,18 @@ import { useState } from 'react';
 
 
 export default function Todo() {
-    const [todo, setTodo] = useState('')
+    const [todo, setTodo] = useState('');
+    const [todos, setTodos] = useState([]);
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        setTodos([...todos, todo]); // spread operator ... copies all previous items
+        setTodo('');
+    }
+
     return (
         <div>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input onChange={e => setTodo(e.target.value)} value={todo} type="text"/>
                 <button type="submit">Add</button>
             </form>

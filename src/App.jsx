5d85768b66b3c366fc71a26bf8d492cb2ccc654b
Artem Penteskul <artemiy.penteskul@gmail.com>
2024-01-21 15:23:12 +0200

    added header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 
+import Header from './components/Header.jsx';
 import Todo from './components/Todo.jsx';
 
 
@@ -8,6 +9,7 @@ export default function App() {
 
   return (
       <div className="App">
+          <Header />
           <Todo />
       </div>
   )

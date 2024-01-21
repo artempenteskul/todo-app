351755f38b7d7d4c51f8883e21c2de2452c9ee40
Artem Penteskul <artemiy.penteskul@gmail.com>
2024-01-21 15:31:11 +0200

    added styling for header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,7 @@
+import styles from '../css/header.module.css';
+
 export default function Header() {
     return (
-        <div>My Todo List</div>
+        <h1 className={styles.header}>My Todo List</h1>
     );
 }
